cf9680e5f74b5ac162bfd9bfc7eeeb2a241e1e02
seyikayode <oluwaseyikayode25@gmail.com>
2020-11-01 01:22:58 +0100

    deploy

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 from dotenv import load_dotenv
 load_dotenv()
